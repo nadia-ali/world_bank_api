19d9d992440ba6269d9aa3f9db8171365617fb9c
Nadia Ali <nadiaali95@live.co.uk>
2021-04-10 22:21:30 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+# in-built
+
+# 3rd party
+
+def get_countries_data():
+    """ Retrieve countries data from World Bank Country API """
+    return None
+
+
+def get_gdp_data():
+    """ Retrieve GDP data from World Bank Data Catalog """
+    return None
+
+# Store data in PostgreSQL database
+
+# Model data to perform analysis
+
+# Write SQL queries to questions
+
+
+if __name__ == '__main__':
+    get_countries_data()
+    get_gdp_data()
